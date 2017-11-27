9e1449eae80b834f0c91b15fa533ba4bbd55c43f
Arushi Singhal <arushisinghal@gmail.com>
2017-11-27 22:39:29 +0530

    automate the noise_data

--- noise_add.m
+++ noise_add.m
@@ -1,15 +1,41 @@
-folder = '/media/minato/Data/SEM 5/SMAI/Final_project/Waves/trumpet';
-audio_files = dir(fullfile(folder,'*.wav'));
-len = numel(audio_files);
+close all;
+clear all;
+prompt = 'enter directory path of the dataset of each instrument: ';
+directory_in=input(prompt,'s');
+%directory_in = '/home/simran/SMAI/Learning Instrument Recognition/Dataset/Waves/';
 
-for i = 1:len
-    filename = audio_files(i).name;
-    b = (fullfile(folder,filename));
+prompt2 = 'enter directory path for the noisy data of each instrument: ';
+directory_noisy=input(prompt2,'s');
 
-    % Reading .wav file
-    [y,Fs] = audioread(b);
-    zz = randn(size(y))/50;
-    yy = y + zz;
-    c = ['/media/minato/Data/SEM 5/SMAI/Final_project/noise_waves/trumpet/',filename];
-    audiowrite(c,yy,Fs);
+prompt1 = 'enter filename of the dataset file: ';
+directory_dataset =input(prompt1,'s');
+%directory_dataset = '/home/simran/SMAI/Learning Instrument Recognition/dataset.csv';
+
+fid = fopen( directory_dataset, 'w' );
+
+dir(directory_in);
+d = dir(directory_in);
+isub = [d(:).isdir];
+nameFolds = {d(isub).name}';
+nameFolds(ismember(nameFolds,{'.','..'})) = [];
+
+for i = 1:size(nameFolds)
+nums = [nameFolds{i,:}];
+s = strcat(directory_in,nums);
+
+allFiles = dir(s);
+allNames = {allFiles.name};
+allNames(ismember(allNames,{'.','..'})) = [];
+
+[rows, columns] = size(allNames);
+
+for j = 1:columns
+
+ audio_path = strcat(s,'/',[allNames{:,j}]);
+ [y,Fs] = audioread(audio_path);
+ zz = randn(size(y))/50;
+ yy = y + zz;
+ c = [directory_noisy,[allNames{:,j}]];
+ audiowrite(c,yy,Fs);
+end
 end
